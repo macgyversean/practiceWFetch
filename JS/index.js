5f7642d2e91821976a9d45972681363eb800f1f4
Sean Carroll <macgyversean@gmail.com>
2024-02-08 14:00:31 -0500

    fixed to accept a search result

--- JS/index.js
+++ JS/index.js
@@ -4,12 +4,15 @@ document.addEventListener("DOMContentLoaded", function () {
   console.log("Content Loaded");
   const userSearch = document.getElementById("userSearch");
   const UsernameInput = document.getElementById("username");
-  const username = UsernameInput.textContent;
+  const username = UsernameInput.value;
+  UsernameInput.textContent = username.value;
   console.log(userSearch);
   console.log(UsernameInput);
-  console.log(username);
-  function GetUserInfo({ userName }) {
-    const url = `https://api.github.com/users/macgyversean`;
+
+  function GetUserInfo(username) {
+    const url = `https://api.github.com/users/${username}`;
+    console.log(username);
+    console.log(url);
     fetch(url, {
       method: "get",
       headers: {
@@ -23,10 +26,11 @@ document.addEventListener("DOMContentLoaded", function () {
         return data;
       });
   }
-
   function updateValue(e) {
     UsernameInput.textContent = e.target.value;
   }
   UsernameInput.addEventListener("input", updateValue);
-  userSearch.addEventListener("click", () => GetUserInfo({ username }));
+  userSearch.addEventListener("click", function () {
+    GetUserInfo(UsernameInput.value);
+  });
 });
